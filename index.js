a15d0db05a5b8429165bc49bf816e6daf2d23f74
Abinaya V <139907637+Abinayavelmurugan@users.noreply.github.com>
2024-12-14 22:52:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
-
+const db=new pg.Client({
+  user:"postgres",
+  host:"localhost",
+  database:"world",
+  password:"22-Feb-05",
+  port:5432
+});
 const app = express();
 const port = 3000;
-
-const db=new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "22-Feb-05",
-  port: 5432,
-});
 db.connect();
-let quiz=[];
-db.query("select * from flags" ,(err,res)=>{
+ 
+
+
+
+let quiz = [];
+db.query("SELECT * FROM capitals",(err,res)=>{
   if(err){
-    console.error("error",err.stack);
+    console.error(err.stack);
   }
-  else{
+  else {
     quiz=res.rows;
   }
-  db.end();
 });
 
 let totalCorrect = 0;
@@ -34,9 +35,9 @@ app.use(express.static("public"));
 let currentQuestion = {};
 
 // GET home page
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   totalCorrect = 0;
-  nextQuestion();
+  await nextQuestion();
   console.log(currentQuestion);
   res.render("index.ejs", { question: currentQuestion });
 });
@@ -45,7 +46,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
@@ -59,8 +60,9 @@ app.post("/submit", (req, res) => {
   });
 });
 
-function nextQuestion() {
+async function nextQuestion() {
   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
   currentQuestion = randomCountry;
 }
 
